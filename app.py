fa7a89d2fe87189d6cdf4868020760cac2ac02b7
TheParadox20 <onyambusamson@gmail.com>
2023-01-22 20:20:12 +0300

    connect db

--- app.py
+++ app.py
@@ -21,3 +21,30 @@ def serve(path):
 @app.route("/test")
 def test():
     return {"test":"Hello World"}
+
+@app.route("/retrive")
+def retrive():
+    report = []
+    wards = cur.execute(f"SELECT DISTINCT ward from Progress").fetchall()
+    # wards = cur.execute(f"SELECT DISTINCT ward from Progress").fetchall()[0][0].split(',')
+    for ward in wards:
+        verified,scanned,uploaded = 0,0,0
+        for update in cur.execute(f"SELECT verified,scanned,uploads FROM Progress WHERE ward=='{ward[0]}'"):
+            uploaded+=update[2]
+            scanned+=update[1]
+            verified+=update[0]
+        report.append(dict(
+            name = ward,
+            verified = verified,
+            scanned = scanned,
+            uploaded = uploaded,
+        ))
+    return {"report":report}
+
+#CREATE TABLE Progress (name varchar, ward char, verified int, scanned int, uploads int, comments varchar, time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL);
+@app.route("/update")
+def update():
+    data = request.args
+    cur.execute(f"INSERT INTO Progress (name, ward, verified, scanned,uploads,comments) VALUES ('{data.get('name')}','{data.get('ward')}','{data.get('verified')}','{data.get('scanned')}','{data.get('upload')}','{data.get('comments')}')")
+    con.commit()
+    return {"ACK":"OK"}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
+import {baseURL} from './data.json'
 import './App.css'
 
 export default function App(){
@@ -8,11 +9,20 @@ export default function App(){
   let [scanned,setScanned] = useState('');
   let [uploaded,setUploaded] = useState('');
   let [comments,setComments] = useState('');
+  let [report, setReport] = useState([{'name':'alpha','verified':'0','scanned':'0','uploaded':'0'}]);
+
+  useEffect(()=>{
+    fetch(baseURL + "/retrive").then((response) => response.json())
+    .then((data) => {
+      setReport(data.report)
+    })
+    .catch((error) => console.log(error));
+  },[])
   let date = new Date()
 
   let update = (e) =>{
     e.preventDefault();
-    let parameters = "?name="+name+"&ward="+ward+"&verified="+verified;
+    let parameters = "?name="+name+"&ward="+ward+"&verified="+verified+"&scanned="+scanned+"&upload="+uploaded+"&comments="+comments;
     fetch(baseURL + "/update" + parameters).then((response) => response.json())
     .then((data) => {
       console.log(data);
@@ -22,7 +32,8 @@ export default function App(){
 
   return(
     <main>
-      <form onSubmit={(e)=>{update(e,betID,alphaStake)}}>
+      <h2>Progress Update</h2>
+      <form onSubmit={(e)=>{update(e)}}>
         <div id='dateTime'>{date.toDateString()}</div>
         <input type="text" placeholder='Name' value={name} onChange={(event)=>{setName(event.target.value)}} />
         <input type="text" placeholder='Ward Name' value={ward} onChange={(event)=>{setWard(event.target.value)}} />
@@ -32,6 +43,29 @@ export default function App(){
         <input type="text" placeholder='Comments' value={comments} onChange={(event)=>{setComments(event.target.value)}} />
         <button type='submit'>Update</button>
       </form>
+      <h2>Progress Report</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Ward Name</th>
+            <th>Verified</th>
+            <th>Scanned</th>
+            <th>Uploaded</th>
+          </tr>
+        </thead>
+        <tbody>
+          {report.map(
+            i=>(
+              <tr>
+                <td>{i.name}</td>
+                <td>{i.verified}</td>
+                <td>{i.scanned}</td>
+                <td>{i.uploaded}</td>
+              </tr>
+            )
+          )}
+        </tbody>
+      </table>
     </main>
   )
 }
