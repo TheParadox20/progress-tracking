d848c4cd37a47a0db9abedaa8b864bcf7ac8f81f
TheParadox20 <onyambusamson@gmail.com>
2023-01-23 00:15:08 +0300

    async

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,8 @@ import './App.css'
 
 export default function App(){
   let [name,setName] = useState('');
-  let [ward,setWard] = useState('');
+  let [ward,setWard] = useState('Ngariama');
+  let [subC,setSubc] = useState('Kirinyaga East');
   let [verified,setVerified] = useState('');
   let [scanned,setScanned] = useState('');
   let [uploaded,setUploaded] = useState('');
@@ -12,31 +13,69 @@ export default function App(){
   let [report, setReport] = useState([{'name':'alpha','verified':'0','scanned':'0','uploaded':'0'}]);
 
   useEffect(()=>{
-    fetch(baseURL + "/retrive").then((response) => response.json())
+    
+  },[])
+  fetch(baseURL + "/retrive").then((response) => response.json())
     .then((data) => {
       setReport(data.report)
     })
     .catch((error) => console.log(error));
-  },[])
   let date = new Date()
+
+  let choices = [
+    ['Kirinyaga East','Kirinyaga West','Mwea West','Mwea east','Kirinyaga Central'],
+    ['1. Ngariama','2. Kabare','3. Baragwi','4. Njukiini','5. Karumandi '],
+    ['1. Mukure','2. Kariti','3. Kiine'],
+    ['1. Thiba','2. Wamumu','3. Kangai','4. Mutithi'],
+    ['1.Nyangati','2. Murinduko','3. Gathigiriri','4.Tebere '],
+    ['1. Kerugoya','2. Kanyekiine','3.Mutira','4. Inoi']
+  ]
 
   let update = (e) =>{
     e.preventDefault();
+
+    setName('');
+    setWard('Ngariama')
+    setSubc('Kirinyaga East')
+    setVerified('')
+    setScanned('')
+    setUploaded('')
+    setComments('')
+
     let parameters = "?name="+name+"&ward="+ward+"&verified="+verified+"&scanned="+scanned+"&upload="+uploaded+"&comments="+comments;
+    console.log(parameters)
     fetch(baseURL + "/update" + parameters).then((response) => response.json())
     .then((data) => {
       console.log(data);
+      window.alert('Sent :)');
     })
-    .catch((error) => console.log(error));  
+    .catch((error) => {console.log(error);window.alert('ERROR :(');});  
   }
 
   return(
     <main>
       <h2>Progress Update</h2>
-      <form onSubmit={(e)=>{update(e)}}>
+      <form onSubmit={(e)=>{update(e);}}>
         <div id='dateTime'>{date.toDateString()}</div>
         <input type="text" placeholder='Name' value={name} onChange={(event)=>{setName(event.target.value)}} />
-        <input type="text" placeholder='Ward Name' value={ward} onChange={(event)=>{setWard(event.target.value)}} />
+        <select value={subC} onChange={event =>{setSubc(event.target.value)}} >
+            {
+              choices[0].map(
+                i=>(
+                  <option value={i}>{i}</option>
+                )
+              )
+            }
+        </select>
+        <select value={ward} onChange={event =>{setWard(event.target.value)}} >
+            {
+              choices[choices[0].indexOf(subC)+1].map(
+                i=>(
+                  <option value={i}>{i}</option>
+                )
+              )
+            }
+        </select>
         <input type="text" placeholder='Documents Verified' value={verified} onChange={(event)=>{setVerified(event.target.value)}} />
         <input type="text" placeholder='Documents Scanned' value={scanned} onChange={(event)=>{setScanned(event.target.value)}} />
         <input type="text" placeholder='Documents Uploaded' value={uploaded} onChange={(event)=>{setUploaded(event.target.value)}} />
